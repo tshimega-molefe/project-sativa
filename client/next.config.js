51e16d30f160c0edbeff361aef4d401fdc6b038d
tshimegabelmont <tshimegabelmont@gmail.com>
2023-12-10 22:56:48 +0200

    Production v1.5

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "https://project-sativa-production.up.railway.app/",
-      },
-    ],
+    domains: ["localhost", "https://project-sativa-production.up.railway.app/"],
   },
 };
 
