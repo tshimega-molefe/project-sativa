8f9ecfec0ece8bceef11fc93fab928467f5d11ec
tshimegabelmont <tshimegabelmont@gmail.com>
2023-12-10 22:54:49 +0200

    Production v1.5

--- client/next.config.js
+++ client/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "digitalhippo-production.up.railway.app",
+        hostname: "https://project-sativa-production.up.railway.app/",
       },
     ],
   },
