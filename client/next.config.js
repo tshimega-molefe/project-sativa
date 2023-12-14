5cbd8ee4bccaeff1400591e9d46c9b533cca0200
tshimegabelmont <tshimegabelmont@gmail.com>
2023-12-14 10:22:38 +0200

    FIX: Updated Sativa URL in NextConfig and Utils

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "http://sativa.up.railway.app"],
+    domains: ["localhost", "http://sativa-sa.vercel.app"],
   },
 };
 
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -58,7 +58,7 @@ export function constructMetadata({
       creator: "@sativa-inc",
     },
     icons,
-    metadataBase: new URL("http://sativa.up.railway.app"),
+    metadataBase: new URL("http://sativa-sa.vercel.app"),
     ...(noIndex && {
       robots: {
         index: false,
