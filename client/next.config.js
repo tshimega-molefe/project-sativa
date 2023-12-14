015d0a4e06f9e17d69cec6313712367d212ab797
tshimegabelmont <tshimegabelmont@gmail.com>
2023-12-14 15:43:57 +0200

    FIX: Updated URL in Next.Config

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "sativa.up.railway.app",
-      },
-    ],
+    domains: ["localhost", "sativa.up.railway.app"],
   },
 };
 
